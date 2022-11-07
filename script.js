8bb9bffd1784666148a762bf6d07ce3fabe1ab6a
Jon Farmer <91025104+jonfarmer@users.noreply.github.com>
2022-11-07 14:33:59 -0500

    Restructure HTML, flesh out CSS, create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const ratingButton = document.querySelectorAll('[data-rating]')
+const submitButton = document.querySelector('[submit]')
+
+let ratingSelection = [];
+
+
+ratingButton.forEach(button => {
+    button.addEventListener('click', ()=> {
+        if 
+    })
+})
